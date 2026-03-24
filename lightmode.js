249e6002c88ee7f8734277596eeed71fb9d81d00
tanvi-cg <tanvichoudhary1528@gmail.com>
2026-03-24 09:27:05 +0530

    Add files via upload

---
+++ lightmode.js
@@ -0,0 +1,21 @@
+let lightmode = localStorage.getItem('ligthmode')
+const themeSwitch = document.getElementById("theme-switch")
+
+const enableLightmode = () => {
+    document.body.classList.add('lightmode')
+    localStorage.setItem('lightmode','active')
+}
+
+
+const disableLightmode = () => {
+    document.body.classList.remove('lightmode')
+    localStorage.setItem('lightmode',null)
+}
+
+if(lightmode === 'active') enableLightmode()
+
+
+themeSwitch.addEventListener("click", () =>{
+    lightmode =  localStorage.getItem('lightmode')
+    lightmode !== "active" ? enableLightmode() : disableLightmode()
+})
